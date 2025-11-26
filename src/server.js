03c63bec06f44b71dc5b24062b2d9484e704aa89
SergiiPavlov <pavlovsergii106@gmail.com>
2025-11-26 12:55:51 +0200

    Align middleware order and username sync

--- src/server.js
+++ src/server.js
@@ -18,8 +18,8 @@ const PORT = process.env.PORT ?? 3000;
 
 // Global middleware
 app.use(logger);
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
 // Routes
